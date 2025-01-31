234e440650e7b692061d05d764427e896570df63
Eliezer Ivantsov <ivancovs982@gmail.com>
2025-01-31 07:22:35 +0200

    update content

--- src/components/content/hero.jsx
+++ src/components/content/hero.jsx
@@ -1,7 +1,7 @@
 import main_hero from '../../assets/main.jpg'
 const Hero = () => {
     return (
-        <section className={"float-left w-1/4 mr-4"}>
+        <section className={"float-left w-1/4 mr-2"}>
             <img className="w-full" src={main_hero} alt="Hero" />
         </section>
     )
